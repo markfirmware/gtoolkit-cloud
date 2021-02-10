72b49f5bfd13f27aca6e9a9e92773af9b4ca26fe
markfirmware <markfirmware@users.noreply.github.com>
2021-02-09 23:34:05 -0500

    remove custom docker file

--- get-gtoolkit.sh
+++ get-gtoolkit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=v0.8.457
+version=v0.8.472
 
 wget -q https://dl.feenk.com/gt/GlamorousToolkitLinux64-$version.zip
 unzip -q Glamorous*.zip
--- gitpod-gtoolkit.dockerfile
+++
@@ -1,4 +0,0 @@
- FROM gitpod/workspace-full-vnc
- 
- # better display when seen from tablet
- RUN sudo sed -i s/1920x1080/1200x900/ /usr/bin/start-vnc-session.sh
