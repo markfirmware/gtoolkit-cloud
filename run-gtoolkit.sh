90dbd97d17291da72dbb7253787663f61d6d2602
markfirmware <mark.kendrat@gmail.com>
2021-02-17 18:37:55 +0000

    install newer libstdc++

--- run-gtoolkit.sh
+++ run-gtoolkit.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+NEW_LIB=newer-libstdc++.so.6
+LIB_FOLDER=/usr/lib/x86_64-linux-gnu
+sudo cp docker/$NEW_LIB $LIB_FOLDER
+sudo ln -sf $LIB_FOLDER/$NEW_LIB $LIB_FOLDER/libstdc++.so.6
+
 cd Glamorous*/
 ./glamoroustoolkit GlamorousToolkit.image --no-quit --interactive
